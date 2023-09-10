65582a5a169eee6365e92af6620dc14772f2e055
Rajiv Shrestha <rajivnewar123@gmail.com>
2023-09-10 10:00:54 +0545

    bug fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://localhost:5173",
+    origin: "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT"],
   })
 );
